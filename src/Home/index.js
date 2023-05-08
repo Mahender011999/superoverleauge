f19465a0e14d7f2fa9968e21a6ef372b10effbad
mahender011999 <mahender011999@gmail.com>
2023-05-08 12:33:32 +0530

    first commit

---
+++ src/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+const index = () => {
+  return (
+    <div className='container'   >
+      <h1 className='heading'>Super Over League</h1>
+      <div>
+      <center>
+      <img  src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" alt="" className='image'/>
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" alt="" className='image'/>
+      </center>
+      </div>
+    </div>
+  )
+}
+
+export default index
